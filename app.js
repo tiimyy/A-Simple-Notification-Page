4f85652a40e15b2823b822025197804a29a74d62
Tiimyy <danieltimileyin00@gmail.com>
2023-01-03 03:26:30 -0800

    second

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const sections=document.querySelectorAll('section')
 const read=document.querySelector('.read')
 const notification=document.querySelector('span')
+const red=document.querySelectorAll('.red')
 
 
 read.addEventListener('click',function(e){
     e.preventDefault()
  sections.forEach(mov=>mov.classList.remove('unread'))
  notification.classList.remove('num')
+ red.forEach(red=>red.style.background='white')
  notification.innerHTML=''
 
 })
